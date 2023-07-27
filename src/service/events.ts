48c38388931b981a7e7acb5bafdbc39ae6811682
Ava <ava.soyeon.kim@gmail.com>
2023-07-27 21:48:54 +0900

    Adjust events.ts in Service so that only upcoming events upto 5 items can be returned

--- src/service/events.ts
+++ src/service/events.ts
@@ -10,11 +10,23 @@ export type Event = {
   dues: string;
 };
 
+function filterUpcomingItems(item: Event) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const finishDate = new Date(item.finishEvent);
+  finishDate.setHours(0, 0, 0, 0);
+
+  return finishDate >= today;
+}
+
 export async function getAllPosts(): Promise<Event[]> {
   const filePath = path.join(process.cwd(), 'data', 'events.json');
-  return readFile(filePath, 'utf-8')
-    .then<Event[]>(JSON.parse)
-    .then((events) =>
-      events.sort((a, b) => (a.beginEvent > b.beginEvent ? -1 : 1))
-    );
+  return (
+    readFile(filePath, 'utf-8')
+      .then<Event[]>(JSON.parse)
+      // .then((events) =>
+      //   events.sort((a, b) => (a.beginEvent > b.beginEvent ? -1 : 1))
+      // )
+      .then((events) => events.filter(filterUpcomingItems).slice(0, 5))
+  );
 }
