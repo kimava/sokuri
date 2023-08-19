63d361895d2bb1cab40cc7fe3ebe9e34ab7c7f7f
Ava <ava.soyeon.kim@gmail.com>
2023-08-19 19:06:50 +0900

    :construction: WIP - Add RecruitPost section to plogging

--- src/app/plogging/page.tsx
+++ src/app/plogging/page.tsx
@@ -1,11 +1,13 @@
 import EventBanner from '@/components/EventBanner';
 import EventPost from '@/components/EventPost';
+import RecruitPost from '@/components/RecruitPost';
 
 export default function PloggingPage() {
   return (
     <>
       <EventPost />
       <EventBanner />
+      <RecruitPost />
     </>
   );
 }
