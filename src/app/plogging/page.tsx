859271779e74379fcae79ab36fa6756b00f39843
Ava <ava.soyeon.kim@gmail.com>
2023-10-01 00:59:38 +0900

    :sparkles: Add ploggingPost section to plogging page

--- src/app/plogging/page.tsx
+++ src/app/plogging/page.tsx
@@ -1,6 +1,7 @@
 import EventBanner from '@/components/EventBanner';
 import EventPost from '@/components/EventPost';
 import RecruitPost from '@/components/RecruitPost';
+import PloggingPost from '@/components/PloggingPost';
 
 export default function PloggingPage() {
   return (
@@ -8,6 +9,7 @@ export default function PloggingPage() {
       <EventPost />
       <EventBanner />
       <RecruitPost />
+      <PloggingPost />
     </div>
   );
 }
