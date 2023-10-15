bd99f3e9fa4eeebc2b6d5b6b0e738337f86330c7
Ava <ava.soyeon.kim@gmail.com>
2023-10-15 18:07:21 +0900

    Sort event list by date order

--- src/service/events.ts
+++ src/service/events.ts
@@ -23,9 +23,10 @@ function filterUpcomingItems(item: Event) {
 
 export async function getAllPosts(): Promise<Event[]> {
   const filePath = path.join(process.cwd(), 'data', 'events.json');
-  return readFile(filePath, 'utf-8').then<Event[]>(JSON.parse);
-  // .then((events) =>
-  //   events.sort((a, b) => (a.beginEvent > b.beginEvent ? -1 : 1))
-  // )
+  return readFile(filePath, 'utf-8')
+    .then<Event[]>(JSON.parse)
+    .then((events) =>
+      events.sort((a, b) => (a.beginEvent > b.beginEvent ? -1 : 1))
+    );
   // .then((events) => events.filter(filterUpcomingItems).slice(0, 5));
 }
