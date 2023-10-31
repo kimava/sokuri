235f9a860b7bca2f1685820e8771454d570b7a05
Ava <ava.soyeon.kim@gmail.com>
2023-10-31 21:58:02 +0900

    Add EventPostTab to EventPostPage

--- src/components/EventPostContentSection.tsx
+++ src/components/EventPostContentSection.tsx
@@ -1,6 +1,7 @@
 import { EventDetailWithMeta } from '@/service/events';
 import Image from 'next/image';
 import React from 'react';
+import EventPostTab from './EventPostTab';
 
 type Props = {
   post: EventDetailWithMeta;
@@ -8,33 +9,38 @@ type Props = {
 
 export default function EventPostContentSection({ post }: Props) {
   return (
-    <div className='px-4 py-8 text-left'>
-      <section className='mb-15 '>
-        <h3 className='H300 text-gray-09'>상세 정보</h3>
-        <p className='mt-4 CONTENT text-gray-09 whitespace-pre-line'>
-          {post.content}
-        </p>
-        {post.images && (
-          <div className='mt-7'>
-            {post.images.map((image) => (
-              <div className='relative mb-2 w-full h-[486px] rounded-lg'>
-                <Image
+    <div>
+      <EventPostTab contentId='#content' locationId='#location' />
+      <div className='px-4 py-8 text-left'>
+        <section id='content' className='mb-15'>
+          <h3 className='H300 text-gray-09'>상세 정보</h3>
+          <p className='mt-4 CONTENT text-gray-09 whitespace-pre-line'>
+            {post.content}
+          </p>
+          {post.images && (
+            <div className='mt-7'>
+              {post.images.map((image) => (
+                <div
                   key={image}
-                  src={image}
-                  alt={image}
-                  fill
-                  sizes='100%'
-                  className='rounded-lg'
-                />
-              </div>
-            ))}
-          </div>
-        )}
-      </section>
-      <section>
-        <h3 className='mb-4 H300 text-gray-09'>위치</h3>
-        <p className='mb-4 CONTENT text-gray-09'>{post.venue}</p>
-      </section>
+                  className='relative mb-2 w-full h-[486px] rounded-lg'
+                >
+                  <Image
+                    src={image}
+                    alt={image}
+                    fill
+                    sizes='100%'
+                    className='rounded-lg'
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+        </section>
+        <section id='location' className='scroll-smooth'>
+          <h3 className='mb-4 H300 text-gray-09'>위치</h3>
+          <p className='mb-4 CONTENT text-gray-09'>{post.venue}</p>
+        </section>
+      </div>
     </div>
   );
 }
