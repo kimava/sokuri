1a1eb75dbf9d4c07f6c85b42f0bf41d999d00003
Ava <ava.soyeon.kim@gmail.com>
2023-11-01 00:05:19 +0900

    :sparkles: Add KaKaoMap component

---
+++ src/components/KakaoMap.tsx
@@ -0,0 +1,37 @@
+import Script from 'next/script';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+type Props = {
+  position: {
+    lat: number;
+    lng: number;
+  };
+};
+
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
+
+const KakaoMap = ({ position }: Props) => {
+  return (
+    <>
+      <Script src={KAKAO_SDK_URL} strategy='beforeInteractive' />
+      <Map center={position} className='w-full h-full rounded-lg' level={3}>
+        <MapMarker
+          position={position}
+          image={{
+            // 이미지 url 변경 필요
+            src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+            size: { width: 28, height: 38 },
+            options: {
+              offset: {
+                x: 14,
+                y: 38,
+              },
+            },
+          }}
+        />
+      </Map>
+    </>
+  );
+};
+
+export default KakaoMap;
