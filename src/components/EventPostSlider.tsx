9f100bfb4f0474adec249d3d985a9a5d8d8994cb
Ava <ava.soyeon.kim@gmail.com>
2023-11-13 23:05:06 +0900

    Add link to event post slider on community tab

--- src/components/EventPostSlider.tsx
+++ src/components/EventPostSlider.tsx
@@ -2,6 +2,7 @@
 
 import { Event } from '@/service/events';
 import { countDDay, formatDate } from '@/util/util';
+import Link from 'next/link';
 
 type Props = { posts: Event[] };
 
@@ -9,7 +10,8 @@ export default function EventPostSlider({ posts }: Props) {
   return (
     <>
       {posts.map((post) => (
-        <div
+        <Link
+          href={`/community/event/${post.id}`}
           key={post.title}
           className='my-4 mr-2 pt-4 pb-5 px-4.5 flex-embla min-w-0 text-left bg-white rounded-2.5xl shadow'
         >
@@ -35,7 +37,7 @@ export default function EventPostSlider({ posts }: Props) {
             {formatDate(post.beginEvent)} ~ {formatDate(post.finishEvent)}
           </p>
           <p className='B300 text-gray-06'>{post.organizer}</p>
-        </div>
+        </Link>
       ))}
       <div className='min-w-full flex justify-center items-center'>
         <span className='px-4 py-3 bg-gray-09 rounded-3xl shadow-md S500 text-center text-white'>
