c15399f27d662e241043c1df0a8578bb72227c8d
Ava <ava.soyeon.kim@gmail.com>
2023-12-03 18:39:58 +0900

    Add recruit service functions

--- src/service/recruits.ts
+++ src/service/recruits.ts
@@ -12,7 +12,39 @@ export type Recruit = {
   maxParticipants: number;
 };
 
+export type RecruitDetail = Recruit & {
+  beginEvent: Date;
+  finishEvent: Date;
+  venue: string;
+  dueDate: Date;
+  numOfPeople: string;
+  content: string;
+  website: string;
+  position: {
+    lat: number;
+    lng: number;
+  };
+};
+
 export async function getAllRecruitPosts(): Promise<Recruit[]> {
+  // const res = await fetch(`${API_ENDPOINT}/community`);
+  const filePath = path.join(process.cwd(), 'data', 'recruits.json');
+
+  return readFile(filePath, 'utf-8')
+    .then<Recruit[]>(JSON.parse)
+    .then((events) =>
+      events.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
+    );
+
+  // return fetch(`${API_ENDPOINT}/community`, { next: { revalidate: 3600 } })
+  //   .then<Recruit[]>((res) => res.json())
+  //   .then((events) =>
+  //     events.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
+  //   )
+  //   .then((events) => events.slice(0, 10));
+}
+
+export async function getTopRecruitPosts(): Promise<Recruit[]> {
   // const res = await fetch(`${API_ENDPOINT}/community`);
   const filePath = path.join(process.cwd(), 'data', 'recruits.json');
 
@@ -29,4 +61,10 @@ export async function getAllRecruitPosts(): Promise<Recruit[]> {
   //     events.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
   //   )
   //   .then((events) => events.slice(0, 10));
+}
+
+export async function getRecruitDetail(id: string): Promise<RecruitDetail> {
+  const filePath = path.join(process.cwd(), 'data', 'recruitDetail.json');
+
+  return readFile(filePath, 'utf-8').then<RecruitDetail>(JSON.parse);
 }
