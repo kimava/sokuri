b97832807a60ee5a5717aa7d83547ef7e57117dc
Ava <ava.soyeon.kim@gmail.com>
2024-01-18 21:49:50 +0900

    :construction: WIP - Kakao Login - change redirect uri

--- src/service/kakaoLogin.ts
+++ src/service/kakaoLogin.ts
@@ -1,4 +1,4 @@
 const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-const REDIRECT_URI = 'http://localhost:3000/mypage/login';
+const REDIRECT_URI = 'https://sokuri.vercel.app/mypage/login';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
