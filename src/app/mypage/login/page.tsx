68a177b2b378f2dc11d92467725b0dddc6120311
Ava <ava.soyeon.kim@gmail.com>
2024-01-18 21:40:56 +0900

    :construction: WIP - Kakao Login

--- src/app/mypage/login/page.tsx
+++ src/app/mypage/login/page.tsx
@@ -1,4 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+
 export default function Kakao() {
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    console.log('code is', code);
+    const login = async () => {
+      await fetch(
+        `http://13.209.208.58:8080/login/oauth2/code/kakao?code=${code}`,
+        {
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+          },
+        }
+      ).then((res) => {
+        console.log(res);
+      });
+    };
+    login();
+  }, []);
+
   return (
     <div className='spinner'>
       <h2>
