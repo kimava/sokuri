fccca74069e10bb91b1fac60921ecd5e69452f71
Ava <ava.soyeon.kim@gmail.com>
2024-10-09 21:14:16 +0900

    Remove unused Eventbanner section

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,4 +1,3 @@
-import EventBanner from '@/features/community/main/EventBanner';
 import EventPost from '@/features/community/main/EventPost';
 import RecruitPost from '@/features/community/main/RecruitPost';
 import PloggingPost from '@/features/community/main/PloggingPost';
@@ -7,7 +6,6 @@ export default function CommunityPage() {
   return (
     <div className='bg-white h-full'>
       <EventPost />
-      <EventBanner />
       <RecruitPost />
       <PloggingPost />
     </div>
