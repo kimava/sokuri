7c5c53a5cfa7bcad4a0c11df63bc270301d109a5
Ava <ava.soyeon.kim@gmail.com>
2024-10-21 12:52:12 +0100

    Simplify GNB code and ensure icons activate on subpaths

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -3,41 +3,43 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import Map from '../../public/svgs/map.svg';
-import Trash from '../../public/svgs/trash.svg';
-import User from '../../public/svgs/user.svg';
-import MapActive from '../../public/svgs/map-active.svg';
-import TrashActive from '../../public/svgs/trash-active.svg';
-import UserActive from '../../public/svgs/user-active.svg';
+import {
+  MapIcon,
+  PloggingIcon,
+  UserIcon,
+  MapActiveIcon,
+  PloggingActiveIcon,
+  UserActiveIcon,
+} from '../../public/svgs';
 
 const menu = [
   {
     href: '/map',
-    icon: <Image src={Map} alt='지도' width={24} height={24} />,
-    activatedIcon: <Image src={MapActive} alt='지도' width={24} height={24} />,
-    iconText: '지도',
+    icon: MapIcon,
+    activeIcon: MapActiveIcon,
+    name: 'MAP',
+    alt: 'map icon',
   },
   {
     href: '/plogging',
-    icon: <Image src={Trash} alt='플로깅' width={24} height={24} />,
-    activatedIcon: (
-      <Image src={TrashActive} alt='플로깅' width={24} height={24} />
-    ),
-    iconText: '플로깅',
+    icon: PloggingIcon,
+    activeIcon: PloggingActiveIcon,
+    name: 'PLOGGING',
+    alt: 'plogging icon',
   },
   {
     href: '/mypage',
-    icon: <Image src={User} alt='마이페이지' width={24} height={24} />,
-    activatedIcon: (
-      <Image src={UserActive} alt='마이페이지' width={24} height={24} />
-    ),
-    iconText: '마이페이지',
+    icon: UserIcon,
+    activeIcon: UserActiveIcon,
+    name: 'MY PAGE',
+    alt: 'my page icon',
   },
 ];
 
 export default function GNB() {
   const pathName = usePathname();
 
+  // FIX ME: font size and padding bottom
   return (
     <nav className='sticky bottom-0 flex justify-around items-center B100 border-t border-[#eeeeee] bg-white'>
       {menu.map((item) => (
@@ -46,13 +48,22 @@ export default function GNB() {
           href={item.href}
           className='pt-[11px] pb-[39px] w-32 flex flex-col items-center'
         >
-          {pathName === item.href ? item.activatedIcon : item.icon}
+          {
+            <Image
+              src={pathName.startsWith(item.href) ? item.activeIcon : item.icon}
+              alt={
+                pathName.startsWith(item.href) ? `active ${item.alt}` : item.alt
+              }
+              width={24}
+              height={24}
+            />
+          }
           <span
             className={`mt-0.5 ${
-              pathName === item.href ? `text-blue-01` : `text-gray-08`
+              pathName.startsWith(item.href) ? `text-blue-01` : `text-gray-08`
             }`}
           >
-            {item.iconText}
+            {item.name}
           </span>
         </Link>
       ))}
