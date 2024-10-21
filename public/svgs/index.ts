c4813a38a9880cb38d2455ec9d11cc6cb013a264
Ava <ava.soyeon.kim@gmail.com>
2024-10-21 12:45:55 +0100

    Configure SVG icons for simplified import

---
+++ public/svgs/index.ts
@@ -0,0 +1,17 @@
+import LogoIcon from './logo.svg';
+import MapIcon from './map.svg';
+import PloggingIcon from './trash.svg';
+import UserIcon from './user.svg';
+import MapActiveIcon from './map-active.svg';
+import PloggingActiveIcon from './trash-active.svg';
+import UserActiveIcon from './user-active.svg';
+
+export {
+  LogoIcon,
+  MapIcon,
+  PloggingIcon,
+  UserIcon,
+  MapActiveIcon,
+  PloggingActiveIcon,
+  UserActiveIcon,
+};
